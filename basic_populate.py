a01fd16c0e741f6787531bb3b893f1c2d82adf12
Samuel Kacer <2093358k@student.gla.ac.uk>
2018-02-28 17:36:10 +0000

    adding a basic population script for helping test the mechanics, since there are bugs in it that need to be fixed

---
+++ basic_populate.py
@@ -0,0 +1,33 @@
+'''very basic population script '''
+import os
+os.environ.setdefault('django_settings_module', 'GroupProject.settings')
+import django
+django.setup()
+
+from bots.models import *
+from bots import mechanics
+
+# create some players
+u = User.objects.get_or_create(username='sam', password="")[0]
+u.save()
+p1 = Player.objects.get_or_create(user=u)[0]
+p1.save()
+
+u = User.objects.get_or_create(username='hannah', password="")[0]
+u.save()
+p2 = Player.objects.get_or_create(user=u)[0]
+p2.save()
+
+u = User.objects.get_or_create(username='jonathan', password="")[0]
+u.save()
+p3 = Player.objects.get_or_create(user=u)[0]
+p3.save()
+
+
+# make a robot for every player
+r = Robot.objects.get_or_create(owner =p1, name='r2d2', type = mechanics.WHEELED, value = 10,speed = 10, armour =5, dodge =1, weapon =5,accuracy =10)[0]
+r.save()
+r = Robot.objects.get_or_create(owner =p2, name='C3P0', type = mechanics.BIPEDAL, value = 10,speed = 5, armour =6, dodge =8, weapon =3,accuracy =5)[0]
+r.save()
+r = Robot.objects.get_or_create(owner =p3, name='HL2', type = mechanics.AERIAL, value = 10,speed = 20, armour =3, dodge =15, weapon =2,accuracy =5)[0]
+r.save()
