36c8a3ef7a7427f2c63ab0be9e2157879d93a5f4
Samuel Kacer <2093358k@student.gla.ac.uk>
2018-03-05 14:16:43 +0000

    Added some basic tests for models and mechanics in bots.tests; Altered basic_populate.py so it could be runused in tests.py

--- basic_populate.py
+++ basic_populate.py
@@ -7,31 +7,38 @@ django.setup()
 from bots.models import *
 from bots import mechanics
 
-print('running basic population script...')
+def run():
+    print('running basic population script...')
+    
+    # create some players
+    u = User.objects.get_or_create(username='sam', password="")[0]
+    u.save()
+    p1 = Player.objects.get_or_create(user=u)[0]
+    p1.save()
+    
+    u = User.objects.get_or_create(username='hannah', password="")[0]
+    u.save()
+    p2 = Player.objects.get_or_create(user=u)[0]
+    p2.save()
+    
+    u = User.objects.get_or_create(username='jonathan', password="")[0]
+    u.save()
+    p3 = Player.objects.get_or_create(user=u)[0]
+    p3.save()
+    
+    
+    # make a robot for every player
+    r1 = Robot.objects.get_or_create(owner =p1, name='r2d2', type = mechanics.WHEELED, value = 10,speed = 10, armour =5, dodge =1, weapon =5,accuracy =10)[0]
+    r1.save()
+    r2 = Robot.objects.get_or_create(owner =p2, name='C3P0', type = mechanics.BIPEDAL, value = 10,speed = 5, armour =6, dodge =8, weapon =3,accuracy =5)[0]
+    r2.save()
+    r3 = Robot.objects.get_or_create(owner =p3, name='HL2', type = mechanics.AERIAL, value = 10,speed = 20, armour =3, dodge =15, weapon =2,accuracy =5)[0]
+    r3.save()
+    
+    # run a battle to add a Battle instance to DB
+    mechanics.battle(r1,r2)
+    print('done.')
 
-# create some players
-u = User.objects.get_or_create(username='sam', password="")[0]
-u.save()
-p1 = Player.objects.get_or_create(user=u)[0]
-p1.save()
-
-u = User.objects.get_or_create(username='hannah', password="")[0]
-u.save()
-p2 = Player.objects.get_or_create(user=u)[0]
-p2.save()
-
-u = User.objects.get_or_create(username='jonathan', password="")[0]
-u.save()
-p3 = Player.objects.get_or_create(user=u)[0]
-p3.save()
-
-
-# make a robot for every player
-r1 = Robot.objects.get_or_create(owner =p1, name='r2d2', type = mechanics.WHEELED, value = 10,speed = 10, armour =5, dodge =1, weapon =5,accuracy =10)[0]
-r1.save()
-r2 = Robot.objects.get_or_create(owner =p2, name='C3P0', type = mechanics.BIPEDAL, value = 10,speed = 5, armour =6, dodge =8, weapon =3,accuracy =5)[0]
-r2.save()
-r3 = Robot.objects.get_or_create(owner =p3, name='HL2', type = mechanics.AERIAL, value = 10,speed = 20, armour =3, dodge =15, weapon =2,accuracy =5)[0]
-r3.save()
-
-print('done.')
+# execution begins here 
+if __name__ == '__main__':
+    run()
--- bots/tests.py
+++ bots/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from bots.models import *
+from bots import mechanics
+import basic_populate
 
 # Create your tests here.
+
+    # helper method 
+def createPlayer():
+    u = User(username='testuser', password="")
+    u.save()
+    p = Player(user=u)
+    p.save()
+    return p
+def createRobot(owner):
+    r = Robot(owner =owner, name='r2d2', type = mechanics.WHEELED, value = 10,speed = 10, armour =5, dodge =1, weapon =5,accuracy =10)
+    return r
+
+class ModelTests(TestCase):
+    ''' tests for models.py'''
+    
+
+    
+    def testCanCreatePlayer(self):
+        p = createPlayer()
+
+    def testCanCreateRobot(self):
+        p = createPlayer()
+        createRobot(p)
+        
+class MechanicsTests(TestCase):
+    def testBattleFuncRuns(self):
+# populate database    
+        basic_populate.run()
+        r1 = Robot.objects.all()[0]
+        r2 = Robot.objects.all()[1]
+        mechanics.battle(r1, r2)
