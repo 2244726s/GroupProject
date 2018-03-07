94ca33344c9ee2509f4f672644cf996bd4770946
Samuel Kacer <2093358k@student.gla.ac.uk>
2018-03-07 16:03:18 +0000

    Added url mapping for profilepage and simple view for it as well.

--- bots/urls.py
+++ bots/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
+from bots import views
 
 urlpatterns = [
-
+    url(r'^profile/(?P<profile_name>[\w\-]+)/$', views.show_profile, name ='show_profile'),]
 ]
--- bots/views.py
+++ bots/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from bots.models import *
+
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'bots/test.html', {})
+    return render(request, 'bots/test.html', {})
+
+def show_profile(request, profile_name):
+    # try to retrieve info about player
+    try:
+        player = Player.objects.get(user__username = profile_name)
+        context = {'player': player}
+    except: # player not found
+        context = {'player': None}
+    
+    return render(request, 'bots/profile.html', context)
