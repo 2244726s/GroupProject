f81d480c1e5ccc6795c777ea41ef47545329670b
Samuel Kacer <2093358k@student.gla.ac.uk>
2018-03-09 17:52:51 +0000

    fixed bug in show_profile() view. Added if player clause to profile .html, to account for if non existant player is trying to be accessed.

--- bots/views.py
+++ bots/views.py
@@ -13,25 +13,22 @@ def show_profile(request, profile_name):
     # try to retrieve info about player
     try:
         player = Player.objects.get(user__username = profile_name)
-        number_of_robots = len(Robot.objects.filter(owner = player))
-        followees = player.follows.all()
-        top5_valuable__robots = Robot.objects.filter(owner=player).order_by('-value')[:5]
-        top5_winning__robots = Robot.objects.filter(owner=player).order_by('-wins')[:5]
+    except: # no player found
+        context = {'player': None}
+
+    if player:
         context = {
             'player': player,
             # number of robots the player owns
-            'number_of_robots' : number_of_robots,
+            'number_of_robots' : len(Robot.objects.filter(owner = player)),
             # players this player is following 
-            'followees' : followees,
+            'followees' : player.follows.all(),
             # the 5 most valuable robots the player owns
-            'top5_valuable_robots' : top5_valuable_robots,
+            'top5_valuable_robots' : Robot.objects.filter(owner=player).order_by('-value')[:5],
             # top5 robots with the most wins
-            'top5_winning_robots' : top5_winning__robots,
+            'top5_winning_robots' : Robot.objects.filter(owner=player).order_by('-wins')[:5],
         }
             
-    except: # player not found
-        context = {'player': None}
-
     return render(request, 'bots/profile.html', context)
 
 def upgrade(request):
